3315d3501d7adc952d471ba304a8edda911184a2
Hisashi KOMINE <komine-hisashi@dmm.com>
2015-02-24 15:46:32 +0900

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'docker'
+maintainer       'Hisashi KOMINE'
+maintainer_email 'hssh@hssh.jp'
+license          'mit'
+description      'Installs/Configures docker'
+long_description 'Installs/Configures docker'
+version          '0.1.0'
+supports         'centos', '7.0'
+
+depends 'selinux'
